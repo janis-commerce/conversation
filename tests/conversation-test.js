767b19f4b1e0411e3d84fd96d2713ac472cf4a61
Manuel Vilche <manuelvilche@gmail.com>
2022-04-22 18:01:18 -0300

    [master] - Initial Commit

---
+++ tests/conversation-test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// const assert = require('assert');
+
+// const sinon = require('sinon');
+
+// const {
+// 	Conversation,
+// 	ConversationError
+// } = require('../lib');
+
+// describe('Conversation', () => {
+
+// 	// your tests here...
+
+// });
